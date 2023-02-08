ad3338042cc39c5a934c2c9a24cc9cfcea4bdd4e
Podverta <podverta@yandex.ru>
2023-02-08 08:01:08 +0300

    add branch

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,17 +3,17 @@
 if [ "1" = "1" ]
 then
   echo "migrate"
-  echo "RUN MIGRATIONS"
+
   python manage.py makemigrations user_selection
   python manage.py migrate
 
 
-  echo "RUN CUSTOM COMMAND"
+  echo "run command"
 
   python manage.py create_users -u
   python manage.py create_users -a
   python manage.py create_users -m
 
 fi
-
 exec "$@"
+
